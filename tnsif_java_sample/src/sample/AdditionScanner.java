17583b91be8bd99f863b9b4630665f2471f562a9
arjun <arjun@Smiley>
2025-09-29 22:38:30 +0530

    Add

---
+++ tnsif_java_sample/src/sample/AdditionScanner.java
@@ -0,0 +1,15 @@
+package sample;
+import java.util.*;
+public class AdditionScanner {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the numbers:");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
