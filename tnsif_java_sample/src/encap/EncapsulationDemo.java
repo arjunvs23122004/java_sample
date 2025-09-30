146687fc27a8f94eed3b1498be08596ddb7c2c66
arjun <arjun@Smiley>
2025-09-30 09:54:11 +0530

    Encap

---
+++ tnsif_java_sample/src/encap/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package encap;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		Execution obj = new Execution();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+
+	}
+
+}
