b046da53a3c4601afed268d6d77711e77cc181f0
arjun <arjun@Smiley>
2025-10-04 10:11:52 +0530

    overriding

---
+++ tnsif_java_sample/src/polymorphism_overriding/Square.java
@@ -0,0 +1,15 @@
+package polymorphism_overriding;
+
+public class Square extends Shape {
+	private double side;
+	public Square(double side) {
+		this.side = side;
+	}
+	public void draw() {
+		System.out.println("Drawing a square with side " + side);
+	}
+	public void erase() {
+		System.out.println("Erasing a square with side " + side);
+	}
+}
+
