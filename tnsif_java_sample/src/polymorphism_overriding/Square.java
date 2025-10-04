eb127f925d322c45c690a116b3703c42644a82ef
arjun <arjun@Smiley>
2025-10-04 11:20:30 +0530

    overriding

--- tnsif_java_sample/src/polymorphism_overriding/Square.java
+++ tnsif_java_sample/src/polymorphism_overriding/Square.java
@@ -1,15 +1,24 @@
 package polymorphism_overriding;
 
 public class Square extends Shape {
-	private double side;
-	public Square(double side) {
-		this.side = side;
-	}
-	public void draw() {
-		System.out.println("Drawing a square with side " + side);
-	}
-	public void erase() {
-		System.out.println("Erasing a square with side " + side);
-	}
+    private double side;
+
+    // Constructor
+    public Square(double side) {
+        this.side = side;
+    }
+
+    // Override draw method
+    @Override
+    public void draw() {
+        System.out.println("Drawing a square with side " + side);
+    }
+
+    // Override erase method
+    @Override
+    public void erase() {
+        System.out.println("Erasing a square with side " + side);
+    }
 }
+
 
--- tnsif_java_sample/src/polymorphism_overriding/Triangle.java
+++ tnsif_java_sample/src/polymorphism_overriding/Triangle.java
@@ -9,9 +9,11 @@ public class Triangle extends Shape {
         this.base = base;
         this.height = height;
     }
+
     public void draw() {
         System.out.println("Drawing a triangle with base " + base + " and height " + height);
     }
+
     public void erase() {
         System.out.println("Erasing a triangle with base " + base + " and height " + height);
     }
